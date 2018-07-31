9705ccf14b9f7f3b1031773c08457a69ecaba70a
inejc <nejc.ilenic@gmail.com>
2018-07-31 10:59:51 +0200

    add initial printable solution

--- src/main/scala/inejc/github/io/printable/Main.scala
+++ src/main/scala/inejc/github/io/printable/Main.scala
@@ -1,5 +1,16 @@
 package inejc.github.io.printable
 
+import inejc.github.io.printable.PrintableSyntax._
+
+final case class Cat(name: String, age: Int, color: String)
+
+object Cat {
+
+  implicit lazy val printableCat: Printable[Cat] = (cat: Cat) =>
+    s"${cat.name.formatToString} is a ${cat.age.formatToString} year-old ${cat.color.formatToString} cat."
+}
+
 object Main extends App {
-  println("works")
+  val cat = Cat("Garfield", 6, "ginger and black")
+  cat.print
 }
--- src/main/scala/inejc/github/io/printable/Printable.scala
+++ src/main/scala/inejc/github/io/printable/Printable.scala
@@ -1,5 +1,32 @@
 package inejc.github.io.printable
 
-trait Printable {
+// type class (definition of the API)
+trait Printable[A] {
 
+  def format(value: A): String
+}
+
+// type class instances (implementations for the types we care about)
+object Printable {
+
+  // implicit lazy val printableString: Printable[String] = new Printable[String] {
+  //
+  //  override def format(value: String): String = value
+  // }
+
+  // or shorter as a single abstract method expression
+  implicit lazy val printableString: Printable[String] = (value: String) => value
+
+  implicit lazy val printableInt: Printable[Int] = (value: Int) => value.toString
+}
+
+// the actual interface exposed to the user using extension methods
+object PrintableSyntax {
+
+  implicit class PrintableOps[A](value: A) {
+
+    def formatToString(implicit p: Printable[A]): String = p.format(value)
+
+    def print(implicit p: Printable[A]): Unit = println(p.format(value))
+  }
 }
